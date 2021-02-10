a02146d91c29181743640ad9303ed276f988ebba
dpwstudio <dpw.contact@gmail.com>
2021-02-10 11:06:44 +0100

    edit environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://nem-shop.fr/api'
+	production: true,
+	apiUrl: 'https://nem-shop.fr/api',
+	keyWSNemShop: 'X47GUML4R3KZEDBH1DPC8LITYVGED659'
+
 };
